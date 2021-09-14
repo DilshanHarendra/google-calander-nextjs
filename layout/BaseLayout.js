a458dc2049f05396ce1cfcfac90ab6495a41d9e1
DilshanHarendra <harendracripto@gmail.com>
2021-09-14 23:03:01 +0530

    create layout

--- layout/BaseLayout.js
+++ layout/BaseLayout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "../components/Header";
+import Navigation from "../components/Navigation";
 
 function BaseLayout(props){
     return(<div className="bg-gray-50 h-screen w-screen overflow-hidden">
         <Header/>
+        <Navigation/>
         <div className="container mx-auto">
             {props.children}
         </div>
